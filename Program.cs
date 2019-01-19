d8ff820d227ba81675465694418aec804e68d6f4
21Swag <39228508+21Swag@users.noreply.github.com>
2019-01-18 17:50:17 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a string: ");
+            char[] Input =  Console.ReadLine().ToCharArray();
+            Array.Reverse( Input );
+            string ReverseInput = new string( Input );
+            Console.WriteLine(ReverseInput);
+        }
+    }
+}
